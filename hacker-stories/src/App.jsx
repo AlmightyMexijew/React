8aab9b7cc22058dd40ae4067adf4072de2564839
almightymexijew <ctepper625@gmail.com>
2023-05-01 23:18:45 +0300

    non-object way via function

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -4,10 +4,13 @@ const welcome = {
   title:"React",
   greeting:"Hey",
 }
+function getTitle(title){
+  return title;
+}
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>Hello {getTitle('React')}</h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text"></input>
     </div>
