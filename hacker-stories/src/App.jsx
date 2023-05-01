3dbbed19338221d16b3fcaad43c02af6703546f6
almightymexijew <ctepper625@gmail.com>
2023-05-01 23:16:01 +0300

    using objects for insertions

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
-const title='React';
+const welcome = {
+  title:"React",
+  greeting:"Hey",
+}
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
-      <p>{title} is my new toy</p>
+      <h1>{welcome.greeting} {welcome.title}</h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text"></input>
     </div>
